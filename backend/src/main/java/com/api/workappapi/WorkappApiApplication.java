85564176e16c3dec4b7eab81a973e191fee74eea
Marcos Diego <>
2022-12-16 21:07:48 -0300

    4emeio
    COMMIT-01 Projeto Original

---
+++ backend/src/main/java/com/api/workappapi/WorkappApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.workappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkappApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/api/workappapi/WorkappapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.workappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkappapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
